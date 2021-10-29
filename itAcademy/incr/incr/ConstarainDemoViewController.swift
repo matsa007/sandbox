6155a971c1eb31348c336dcdf132bfa51ea6af5f
matsa_007 <serg.arbitran@gmail.com>
2021-10-29 20:39:44 +0300

    no message

---
+++ itAcademy/incr/incr/ConstarainDemoViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ConstarainDemoViewController.swift
+//  incr
+//
+//  Created by Сергей Матвеенко on 26.10.2021.
+//
+
+import UIKit
+
+class ConstarainDemoViewController: UIViewController {
+    
+    
+}
